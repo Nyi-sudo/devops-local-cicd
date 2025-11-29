abe2dc274b5885d88c355ab06f87dd5002944b02
g33k <nnpyaehan@gmail.com>
2025-11-29 11:50:10 +0630

    Day 3: Added Jenkinsfile and pipeline setup

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("DevOps CI/CD Pipeline: Day 1 Working!");
+  res.send("Day 2: Docker Deployment Successful!");
 });
 
 app.listen(3000, () => {
