6c8bc1970dbb08e8082cc4447ab98105e88d8da4
g33k <nnpyaehan@gmail.com>
2026-01-04 20:31:25 +0630

    day10

---
+++ terraform-network/ec2.tf
@@ -0,0 +1,14 @@
+data "aws_ssm_parameter" "ubuntu" {
+  name = "/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
+}
+
+resource "aws_instance" "server" {
+  ami                    = data.aws_ssm_parameter.ubuntu.value
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = { Name = "devops-terraform-server" }
+}
+
+
